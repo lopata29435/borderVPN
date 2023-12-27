c16ea4e35487e4da2bdb798f114505d0021bf0d6
lopata29435 <lopata29435@gmail.com>
2023-12-27 18:11:19 +0300

    working version of the extension that sends requests to a vps server running a node js server. For real now

--- servers_extension/scripts/background.js
+++ servers_extension/scripts/background.js
@@ -1,21 +1,29 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "checkConnection") {
-        checkConnection();
-      sendResponse({ status: 'success', message: 'Server connected' });
+        checkConnection()
+            .then(() => {
+                sendResponse({ status: 'success', message: 'Server connected' });
+            })
+            .catch(error => {
+                sendResponse({ status: 'error', message: error.message });
+            });
+        return true;
     }
-    return true;
-  });
-  
-  function checkConnection() {
-    var serverUrl = "http://put in your ip:3000";
-  
-    fetch(serverUrl)
-      .then(response => response.json())
-      .then(data => {
-        console.log("Response from the server:", data);
-      })
-      .catch(error => {
-        console.error("Error during a request to the server:", error);
-      });
-  }
+});
 
+function checkConnection() {
+    var serverUrl = "http://166.1.160.198:3000/";
+
+    return new Promise((resolve, reject) => {
+        fetch(serverUrl)
+            .then(response => response.json())
+            .then(data => {
+                console.log("Response from the server:", data);
+                resolve();
+            })
+            .catch(error => {
+                console.error("Error during a request to the server:", error);
+                reject(new Error("error")); 
+            });
+    });
+}
